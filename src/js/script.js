265f9587d1905b1051107b2d7fd8f1c21648b58e
mariia <m3850816@gmail.com>
2022-02-12 16:21:01 +0700

    first 4 sections

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+var slider = tns({
+    container: ".carousel__inner",
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    // center: true,
+    constrols: false,
+    prevButton: ".slider-prev-btn",
+    nextButton: ".slider-next-btn",
+    nav: false,
+    // navPosition: "bottom"
+});
